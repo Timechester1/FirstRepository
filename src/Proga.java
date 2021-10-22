e000911b2c5cad96ce579df13571fa4093a11600
Timechester1 <yaroslavbeterovv@gmail.com>
2021-10-23 01:16:36 +0700

    Initial commit

---
+++ src/Proga.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Proga {
+    public static void main(String[] args) {
+        System.out.println("Привет как дела");
+        ArrayList<Integer>list = new ArrayList();
+        list.add(4);
+        list.add(42);
+        list.add(41);
+        System.out.println(list.get(1));
+    }
+}
